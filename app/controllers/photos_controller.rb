a76a7d5b21e67798e37cc811c202612e2ea8fb93
AhmedNadar <ahmednadar@gmail.com>
2013-09-05 14:36:16 -0400

    started photo controller

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,23 @@
+class PhotosController < ApplicationController
+	def index
+		@photos = Photo.all
+	end
+	def new
+
+	end
+	def edit
+		
+	end
+	def show
+		
+	end
+	def update
+		
+	end
+	def destroy
+		
+	end
+	def create
+		
+	end
+end
